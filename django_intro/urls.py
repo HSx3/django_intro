044fe55d6822b6c9816f3b5cf9d95f8dc804f7e4
HSx3 <hs.ssafy@gmail.com>
2019-02-14 01:40:48 +0000

    08_Static URL

--- django_intro/urls.py
+++ django_intro/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from home import views # home 디렉토리에 있는 views 불러오기
 
 urlpatterns = [
+    path('home/static_example/', views.static_example, name='static_example'),
     path('home/template_example/', views.template_example, name='template_example'),
     path('home/user_create/', views.user_create, name='user_create'),
     path('home/user_new/', views.user_new, name='user_new'),
